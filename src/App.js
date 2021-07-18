e982cc58779c065a352ff0877f11e70086f25e85
Jade <jadehandsome30@gmail.com>
2021-07-18 15:59:33 +0800

    simple responsive

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
-import React, { useEffect, useState, useCallback } from 'react';
-import Question from './components/Question';
-import CategorySelector from './components/CategorySelector';
-import ResultModal from './components/ResultModal';
-import Scoreboard from './components/Scoreboard';
-import TitleCard from './components/TitleCard';
-import './App.css';
+import React, { useEffect, useState, useCallback } from "react";
+import Question from "./components/Question";
+import CategorySelector from "./components/CategorySelector";
+import ResultModal from "./components/ResultModal";
+import Scoreboard from "./components/Scoreboard";
+import TitleCard from "./components/TitleCard";
+import "./App.css";
 
 export default function App() {
   const [question, setQuestion] = useState(null);
-  const [selectedCategory, setSelectedCategory] = useState('any');
+  const [selectedCategory, setSelectedCategory] = useState("any");
   const [isCorrect, setIsCorrect] = useState(null);
 
   const getQuestion = useCallback(() => {
     setIsCorrect(null);
 
-    let url = 'https://opentdb.com/api.php?amount=10';
-    if (selectedCategory !== 'any') url += `&category=${selectedCategory}`;
+    let url = "https://opentdb.com/api.php?amount=10";
+    if (selectedCategory !== "any") url += `&category=${selectedCategory}`;
 
     fetch(url)
       .then((res) => res.json())
@@ -24,7 +24,7 @@ export default function App() {
 
   useEffect(() => {
     getQuestion();
-  }, [selectedCategory]);
+  }, [getQuestion, selectedCategory]);
 
   function handleQuestionAnswered(answer) {
     const isAnswerCorrect = answer === question.correct_answer;
@@ -32,41 +32,43 @@ export default function App() {
   }
 
   return (
-    <div className="app">
-      {/* show the result modal ----------------------- */}
-      {isCorrect !== null && (
-        <ResultModal
-          isCorrect={isCorrect}
-          getQuestion={getQuestion}
-          question={question}
-        />
-      )}
-
-      <div className="question-header">
-        <TitleCard />
-      </div>
-      {/* question header ----------------------- */}
-      <div className="question-header">
-        <CategorySelector
-          category={selectedCategory}
-          chooseCategory={setSelectedCategory}
-        />
-        <Scoreboard isCorrect={isCorrect} />
-      </div>
-
-      {/* the question itself ----------------------- */}
-      <div className="question-main">
-        {question && (
-          <Question
+    <div className="containing">
+      <div className="app">
+        {/* show the result modal ----------------------- */}
+        {isCorrect !== null && (
+          <ResultModal
+            isCorrect={isCorrect}
+            getQuestion={getQuestion}
             question={question}
-            answerQuestion={handleQuestionAnswered}
           />
         )}
-      </div>
 
-      {/* question footer ----------------------- */}
-      <div className="question-footer">
-        <button onClick={getQuestion}>Go to next question 👉</button>
+        <div className="question-header">
+          <TitleCard />
+        </div>
+        {/* question header ----------------------- */}
+        <div className="question-header">
+          <CategorySelector
+            category={selectedCategory}
+            chooseCategory={setSelectedCategory}
+          />
+          <Scoreboard isCorrect={isCorrect} />
+        </div>
+
+        {/* the question itself ----------------------- */}
+        <div className="question-main">
+          {question && (
+            <Question
+              question={question}
+              answerQuestion={handleQuestionAnswered}
+            />
+          )}
+        </div>
+
+        {/* question footer ----------------------- */}
+        <div className="question-footer">
+          <button onClick={getQuestion}>Go to next question 👉</button>
+        </div>
       </div>
     </div>
   );
